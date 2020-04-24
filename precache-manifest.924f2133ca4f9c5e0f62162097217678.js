c531bb5816982e44cbed2fd736caa4f7cfcca258
Adnan Reza <adnan.reza@outlook.com>
2020-04-24 15:23:50 -0700

    Updates

---
+++ precache-manifest.924f2133ca4f9c5e0f62162097217678.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8855a0337561e0868a9c0ebd40e5196",
+    "url": "/covidTracker/index.html"
+  },
+  {
+    "revision": "3555899cf4ca83f77a49",
+    "url": "/covidTracker/static/css/main.b3d6b097.chunk.css"
+  },
+  {
+    "revision": "7efbca942bfa2a214bb6",
+    "url": "/covidTracker/static/js/2.9ddb2a07.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidTracker/static/js/2.9ddb2a07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3555899cf4ca83f77a49",
+    "url": "/covidTracker/static/js/main.1a7ebe2f.chunk.js"
+  },
+  {
+    "revision": "297080c173d3278ac19d",
+    "url": "/covidTracker/static/js/runtime-main.220a9d5a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidTracker/static/media/covid.d7265326.png"
+  }
+]);
